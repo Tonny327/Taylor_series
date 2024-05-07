0339537e81ae6063ff0a4de78437355e3e63e254
Tonny327 <159911144+Tonny327@users.noreply.github.com>
2024-05-08 01:12:02 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="Taylor_series",
     version="1.0",
     description="Разложение тригонометрических функций в ряд Тейлора",
-    executables=[Executable("Taylor_series.py", base="Win32GUI", icon="icon_for_taylor.ico")],
-    target_dir = r"D:\BGAS_study\Python\kurss"
+    executables=[
+        Executable("Taylor_series.py", base="Win32GUI", icon=r"D:\BGAS_study\Python\kurss\icon_for_taylor.ico")],
 )
